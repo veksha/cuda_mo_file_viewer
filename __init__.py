3d1fb6532f11c91ea0e5f6a67a03c21f0104d900
veksha <veksho@gmail.com>
2023-04-07 17:25:56 +0300

    i18n patch from Markus F.

--- __init__.py
+++ __init__.py
@@ -1,58 +1,60 @@
 import os
 from cudatext import *
-#from cudax_lib import get_translation
-#_   = get_translation(__file__)  # I18N
+from cudax_lib import get_translation
+_ = get_translation(__file__)  # I18N
 
-CAPTION = 'MO File Viewer'
+CAPTION = _('MO File Viewer')
 
 import gettext
 
 class Command:
-    
+
     dialog_visible = False
-    
+
     def __init__(self):
         self.hdlg = None
         self.messages = ''
         self.my_list = []
-        
+
     def open_file(self):
-        self.filename = dlg_file(True, '', '', '*.mo|*.mo', 'Please, provide path to .mo file')
+        self.filename = dlg_file(True, '', '', '*.mo|*.mo', _('Please, provide path to .mo file'))
         if self.filename:
             dlg_proc(self.hdlg, DLG_PROP_SET, prop={'cap': CAPTION + ' - ' + self.filename})
             translation = gettext.GNUTranslations(open(self.filename, 'rb'))
             self.messages = [str(n)+'\r'+k.replace('\t',chr(3))+'\r'+v.replace('\t',chr(3))+'\t' for n, (k, v) in enumerate(translation._catalog.items())]
-            self.messages.insert(0,'Offset\rOriginal\rTranslation\t') # first row is column headers
+            self.messages.insert(0, _('Offset\rOriginal\rTranslation\t')) # first row is column headers
             self.my_list = list(translation._catalog.items())
         return self.filename
-            
+
     def fill_data(self):
         dlg_proc(self.hdlg, DLG_CTL_PROP_SET, name='mylistview', prop={
             'items': ''.join(self.messages),
             'columns': '\t'.join([
-                '\r'.join(['Offset', '70','','','L']),
-                '\r'.join(['Original', '400']),
-                '\r'.join(['Translation', '400']),
+                '\r'.join([_('Offset'), '70','','','L']),
+                '\r'.join([_('Original'), '400']),
+                '\r'.join([_('Translation'), '400']),
                 ]),
             })
+        # Clear the memo panels when loading the (next) file
         dlg_proc(self.hdlg, DLG_CTL_PROP_SET, name='memo1', prop={'val': ''})
         dlg_proc(self.hdlg, DLG_CTL_PROP_SET, name='memo2', prop={'val': ''})
 
     def run(self):
         if self.dialog_visible:
             return
-        
+
         self.hdlg = dlg_proc(0, DLG_CREATE)
-        
+
         def onclose(*args, **kwargs):
             self.dialog_visible = False
-            timer_proc(TIMER_START_ONE, lambda *args, **wargs: dlg_proc(self.hdlg, DLG_FREE), 100)
-        
-        dlg_proc(self.hdlg, DLG_PROP_SET, prop={'cap': 'MO File Viewer', 'w': 920, 'h': 650, 'border': DBORDER_SIZE,
-        'w_min': 400,
-        'h_min': 330,
-        'on_close': onclose})
-        
+            timer_proc(TIMER_START_ONE, lambda *args, **kwargs: dlg_proc(self.hdlg, DLG_FREE), 100)
+
+        dlg_proc(self.hdlg, DLG_PROP_SET, prop={
+            'cap': CAPTION, 'w': 920, 'h': 650, 'border': DBORDER_SIZE,
+            'w_min': 400,
+            'h_min': 330,
+            'on_close': onclose})
+
         n = dlg_proc(self.hdlg, DLG_CTL_ADD, 'memo')
         dlg_proc(self.hdlg, DLG_CTL_PROP_SET, index=n, prop={
             'name': 'memo1',
@@ -72,14 +74,14 @@ class Command:
             'h': 100,
             'ex0': True,
         })
-        
+
         def onclick(id_dlg, id_ctl, data):
             index = data[0]
             selected = data[1]
             if selected:
                 dlg_proc(self.hdlg, DLG_CTL_PROP_SET, name='memo1', prop={'val': self.my_list[index][0].replace('\t',chr(3))})
                 dlg_proc(self.hdlg, DLG_CTL_PROP_SET, name='memo2', prop={'val': self.my_list[index][1].replace('\t',chr(3))})
-        
+
         n = dlg_proc(self.hdlg, DLG_CTL_ADD, 'listview')
         dlg_proc(self.hdlg, DLG_CTL_PROP_SET, index=n, prop={
             'name': 'mylistview',
@@ -87,28 +89,27 @@ class Command:
             'sp_a': 10,
             'a_b':('memo1','['),
             'columns': '\t'.join([
-                '\r'.join(['Offset', '70','','','L']),
-                '\r'.join(['Original', '400']),
-                '\r'.join(['Translation', '400']),
+                '\r'.join([_('Offset'), '70','','','L']),
+                '\r'.join([_('Original'), '400']),
+                '\r'.join([_('Translation'), '400']),
                 ]),
             'on_select': onclick,
             })
-            
+
         self.filename = None
         def onbutton(*args, **kwargs):
             self.open_file() and self.fill_data()
-            
+
         n = dlg_proc(self.hdlg, DLG_CTL_ADD, 'button')
         dlg_proc(self.hdlg, DLG_CTL_PROP_SET, index=n, prop={
             'name': 'mybutton',
-            'cap': 'Open...',
+            'cap': _('Open...'),
             'align': ALIGN_TOP,
             'sp_a': 10,
             'on_change': onbutton,
             })
-        
+
         dlg_proc(self.hdlg, DLG_SCALE)
-        
+
         self.dialog_visible = True
         dlg_proc(self.hdlg, DLG_SHOW_NONMODAL)
-
---
+++ readme/readme.txt
@@ -0,0 +1,6 @@
+CudaText plugin.
+Shows dialog to view contents of *.mo files, used by GNU gettext
+translation system. This system is also used to translate CudaText plugins.
+
+Author: veksha, https://github.com/veksha/
+License: MIT
